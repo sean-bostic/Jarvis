981b2a2c13157364e14e4baeb944a60dc0d09ad3
Sean Bostic <sean.bostic@gmail.com>
2017-04-03 22:50:16 -0400

    Adding GetAllCategories method
    Method will grab all the possible processes on the machine.

--- SystemMonitor/Program.cs
+++ SystemMonitor/Program.cs
@@ -27,7 +27,6 @@ namespace SystemMonitor
 
             #region PerformanceCounters
 
-            
 
             //Make a performance counter to gather CPU information
             PerformanceCounter perfomanceCPU = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
@@ -38,6 +37,8 @@ namespace SystemMonitor
             //Make an uptime counter
             PerformanceCounter performanceUptime = new PerformanceCounter("System", "System Up Time");
             performanceUptime.NextValue();
+
+            //GetAllCounters();
 
 
             TimeSpan uptimeSpan = TimeSpan.FromSeconds(performanceUptime.NextValue());
@@ -98,6 +99,48 @@ namespace SystemMonitor
                 Thread.Sleep(1000);
 
             }
+        }
+
+        private static void GetAllCounters(string categoryFilter)
+        {
+            var categories = PerformanceCounterCategory.GetCategories();
+            foreach (var cat in categories)
+            {
+                if (categoryFilter != null && categoryFilter.Length > 0)
+                {
+                    if (!cat.CategoryName.Contains(categoryFilter)) continue;
+                }
+                Console.WriteLine("Category {0}", cat.CategoryName);
+                try
+                {
+                    var instances = cat.GetInstanceNames();
+                    if (instances != null && instances.Length > 0)
+                    {
+                        foreach (var instance in instances)
+                        {
+                            //if (cat.CounterExists(instance))
+                            //{
+                            foreach (var counter in cat.GetCounters(instance))
+                            {
+                                Console.WriteLine("\tCounter Name {0} [{1}]", counter.CounterName, instance);
+                            }
+                            //}
+                        }
+                    }
+                    else
+                    {
+                        foreach (var counter in cat.GetCounters())
+                        {
+                            Console.WriteLine("\tCounter Name {0}", counter.CounterName);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // NO COUNTERS
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
